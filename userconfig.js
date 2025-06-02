4f5cdc8a4a92dcbb4a11f1998ed39a4e9034cc55
Aevstiel <116822220+Aevstiel@users.noreply.github.com>
2025-06-02 23:02:56 +0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -10,7 +10,7 @@ let palette = initThemeSystem(preferredLightTheme, preferredDarkTheme);
 const default_configuration = {
   overrideStorage: true,
   temperature: {
-    location: "London",
+    location: "Alexandria",
     scale: "C",
   },
   clock: {
