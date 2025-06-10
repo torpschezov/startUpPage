700ea09b0b358711ae3db8c7a588234c64b21411
Aevstiel <116822220+Aevstiel@users.noreply.github.com>
2025-06-10 16:23:29 +0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -1,9 +1,9 @@
 // User configuration for the startpage - update the palette, location, and your preferred tabs, categories, and links
 
 // Define preferred palette for light and dark mode
-// Available themes: latte, frappe, mocha, macchiato
-const preferredLightTheme = latte;
-const preferredDarkTheme = mocha;
+// Available themes: latte, frappe, mocha, macchiato, lycia
+const preferredLightTheme = lycia;
+const preferredDarkTheme = lycia;
 
 let palette = initThemeSystem(preferredLightTheme, preferredDarkTheme);
 
@@ -22,7 +22,7 @@ const default_configuration = {
       label: "ALX",
       timezone: "Africa/Cairo",
       format: "h:i",
-      icon_color: palette.peach,
+      icon_color: palette.mauve,
     },
   ],
   search: {
@@ -37,12 +37,12 @@ const default_configuration = {
   },
   disabled: [],
   localIcons: false,
-  fastlink: "https://www.catppuccin.com",
+  fastlink: "https://www.github.com/Aevstiel",
   openLastVisitedTab: true,
   tabs: [
     {
       name: "astiel",
-      background_url: "src/img/banners/banner_14.gif",
+      background_url: "src/img/banners/lycia-2.jpg",
       categories: [
         {
           name: "bookmarks",
@@ -51,19 +51,19 @@ const default_configuration = {
               name: "Arch Wiki",
               url: "https://wiki.archlinux.org/",
               icon: "brand-wikipedia",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Apple",
               url: "https:///account.apple.com/",
               icon: "brand-apple",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Lyra",
               url: "https:///lyra.it.com/",
               icon: "flare",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
           ],
         },
@@ -74,25 +74,25 @@ const default_configuration = {
               name: "Gmail",
               url: "https://mail.google.com",
               icon: "brand-gmail",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Proton Mail",
               url: "https://calendar.google.com",
               icon: "mailbox",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Sheets",
               url: "https://docs.google.com/spreadsheets",
               icon: "table",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
             {
               name: "Filen",
               url: "https://filen.io",
               icon: "cloud-storm",
-              icon_color: palette.blue,
+              icon_color: palette.lavender,
             },
           ],
         },
@@ -103,31 +103,31 @@ const default_configuration = {
               name: "Defcon",
               url: "https://www.defconlevel.com",
               icon: "alert-hexagon",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Legion",
               url: "https://ildu.com.ua",
               icon: "tank",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Finance",
               url: "https://finance.yahoo.com",
               icon: "chart-dots-2",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
             {
               name: "Грунт",
               url: "https://grnt.media",
               icon: "eye-bolt",
-              icon_color: palette.blue,
+              icon_color: palette.lavender,
             },
             {
               name: "Translate",
               url: "https://translate.google.com",
               icon: "language-katakana",
-              icon_color: palette.red,
+              icon_color: palette.peach,
             },
           ],
         },
@@ -135,7 +135,7 @@ const default_configuration = {
     },
     {
       name: "dev",
-      background_url: "src/img/banners/banner_15.gif",
+      background_url: "src/img/banners/lycia-3.jpg",
       categories: [
         {
           name: "development",
@@ -144,19 +144,19 @@ const default_configuration = {
               name: "Github",
               url: "https://github.com",
               icon: "brand-github",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "ChatGPT",
               url: "https://chatgpt.com",
               icon: "brand-openai",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Stack Overflow",
               url: "https://stackoverflow.com",
               icon: "brand-stackoverflow",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
           ],
         },
@@ -167,25 +167,25 @@ const default_configuration = {
               name: "Kaggle",
               url: "https://www.kaggle.com",
               icon: "brain",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Leetcode",
               url: "https://leetcode.com",
               icon: "code-plus",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Exercism",
               url: "https://exercism.org",
               icon: "code-minus",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
             {
               name: "Chess",
               url: "https://chess.com",
               icon: "chess",
-              icon_color: palette.blue,
+              icon_color: palette.lavender,
             },
           ],
         },
@@ -196,25 +196,25 @@ const default_configuration = {
               name: "Debian Wiki",
               url: "https://wiki.debian.org",
               icon: "brand-debian",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Hacker News",
               url: "https://news.ycombinator.com",
               icon: "brand-redhat",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Apple Wiki",
               url: "https://theapplewiki.com/wiki",
               icon: "brand-apple",
-              icon_color: palette.blue,
+              icon_color: palette.lavender,
             },
             {
               name: "Champagne",
               url: "https://champagne.pages.dev",
               icon: "skull",
-              icon_color: palette.pink,
+              icon_color: palette.peach,
             },
           ],
         },
@@ -222,7 +222,7 @@ const default_configuration = {
     },
     {
       name: "chi ll",
-      background_url: "src/img/banners/banner_13.gif",
+      background_url: "src/img/banners/lycia-1.jpg",
       categories: [
         {
           name: "social media",
@@ -231,25 +231,25 @@ const default_configuration = {
               name: "Instagram",
               url: "https://www.instagram.com",
               icon: "brand-instagram",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Facebook",
               url: "https://www.facebook.com",
               icon: "brand-facebook",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Reddit",
               url: "https://www.reddit.com/r/unixporn",
               icon: "brand-reddit",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
             {
               name: "Dread",
               url: "https://www.reddit.com/r/unixporn",
               icon: "shield-checkered",
-              icon_color: palette.blue,
+              icon_color: palette.lavender,
             },
           ],
         },
@@ -260,25 +260,25 @@ const default_configuration = {
               name: "IGN",
               url: "https://www.ign.com/account/playlist/library",
               icon: "device-gamepad",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Steam",
               url: "https://store.steampowered.com",
               icon: "brand-steam",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Epic Games",
               url: "https://store.epicgames.com",
               icon: "brand-fortnite",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
             {
               name: "Nintendo",
               url: "https://store.nintendo.co.uk",
               icon: "device-nintendo",
-              icon_color: palette.blue,
+              icon_color: palette.lavender,
             },
           ],
         },
@@ -289,25 +289,25 @@ const default_configuration = {
               name: "1Shows",
               url: "https://www.1shows.live",
               icon: "brand-funimation",
-              icon_color: palette.mauve,
+              icon_color: palette.red,
             },
             {
               name: "Youtube",
               url: "https://www.youtube.com",
               icon: "brand-youtube",
-              icon_color: palette.peach,
+              icon_color: palette.mauve,
             },
             {
               name: "Genesis",
               url: "https://libgen.st/",
               icon: "book",
-              icon_color: palette.pink,
+              icon_color: palette.flamingo,
             },
             {
               name: "Anker",
               url: "https://ankergames.net",
               icon: "device-gamepad-2",
-              icon_color: palette.red,
+              icon_color: palette.lavender,
             },
           ],
         },
@@ -320,4 +320,4 @@ const CONFIG = new Config(default_configuration, palette);
 
 const root = document.querySelector(":root");
 root.style.setProperty("--bg", palette.mantle);
-root.style.setProperty("--accent", palette.mauve);
+root.style.setProperty("--accent", palette.red);
